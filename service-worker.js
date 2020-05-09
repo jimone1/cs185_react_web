3eed305984fc1b69b0893a8572f24e4ca9f58e7f
jimeone1 <yiyaowan@ucsb.edu>
2020-05-08 21:00:31 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cs185_react_web/precache-manifest.98e91c85412378b9783d707f341fa63d.js"
+  "/cs185_react_web/precache-manifest.bfc9206bef188ba5e1a0174fa842b420.js"
 );
 
 self.addEventListener('message', (event) => {
